16c52663cc08073ed3ebba9f374a30b837c3eed7
Manish Mani Dinkar <moneyyiiss@gmail.com>
2023-07-23 16:50:02 +0530

    solved

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,29 +6,115 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
-    public Pizza(Boolean isVeg){
+
+    boolean Ealready = false;
+    boolean Talready = false;
+    boolean Balready = false;
+    boolean isBill= false;
+
+
+    public Pizza(Boolean isVeg) {
         this.isVeg = isVeg;
         // your code goes here
+        if (isVeg == true) {
+            this.price = 300;
+
+
+        } else {
+            this.price = 400;
+
+        }
+
+
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return this.price;
     }
 
-    public void addExtraCheese(){
+    public void addExtraCheese() {
         // your code goes here
+
+        if (Ealready == false) {
+
+            price = price + 80;
+
+
+            Ealready = true;
+        }
+
+
     }
 
-    public void addExtraToppings(){
+    public void addExtraToppings() {
         // your code goes here
+
+
+        if (Talready == false) {
+            if (isVeg) {
+
+                price = price + 70;
+
+            } else {
+
+                price = price + 120;
+
+            }
+            Talready = true;
+        }
+
+
     }
 
-    public void addTakeaway(){
+    public void addTakeaway() {
         // your code goes here
+        if (Balready == false) {
+
+            price = price + 20;
+
+            Balready = true;
+        }
     }
 
-    public String getBill(){
+    public String getBill() {
         // your code goes here
-        return this.bill;
+//            bill= Integer.toString(price);
+
+        if(isBill==false) {
+
+
+            if (isVeg) {
+                this.bill = "Base Price Of The Pizza: " + 300 + "\n";
+            } else {
+                this.bill = "Base Price Of The Pizza: " + 400 + "\n";
+            }
+
+            if (Ealready) {
+                this.bill = this.bill + "Extra Cheese Added: " + 80 + "\n";
+            }
+
+            if (Talready) {
+                if (isVeg) {
+
+
+                    this.bill = this.bill + "Extra Toppings Added: " + 70 + "\n";
+                } else {
+
+
+                    this.bill = this.bill + "Extra Toppings Added: " + 120 + "\n";
+                }
+            }
+
+
+            if (Balready) {
+                this.bill = this.bill + "Paperbag Added: " + 20 + "\n";
+            }
+
+
+            this.bill = this.bill + "Total Price: " + price + "\n";
+            isBill=true;
+        }
+        return bill;
+
     }
 }
